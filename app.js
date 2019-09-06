f0113598c09f67cc513a09899e4cea7fb90cb936
ticobrahe <olabiyi.sam@gmail.com>
2019-09-06 13:06:15 +0100

    create recipe model

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Recipe = require("./models/recipe");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://tico:sgzyzVM16wEgbv6T@cluster0-jbvpv.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("succesffully connected to MongoDB Atlass");
+  })
+  .catch(err => {
+    console.log("Unable to connect to MongoDB");
+    console.error(err);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 module.exports = app;
